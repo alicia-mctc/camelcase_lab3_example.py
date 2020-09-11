60901ba9f37ace3908b8ce4a5ac2aa373a43b2ce
Alicia <alicia.nguyen@go.minneapolis.edu>
2020-09-11 10:26:06 -0700

    welcome banner

--- camelcase_lab3_example.py
+++ camelcase_lab3_example.py
@@ -9,7 +9,14 @@ def camelcase(sentence):
     #So this still works on empty strings,strings of length 1
     return upper_camel_cased[0:1].lower() + upper_camel_cased[1:]
 
+def banner():
+    """Display welcome banner"""
+    message = "CAMELCASE PROGRAM"
+    stars = '*' * len(message) 
+    print(f'{stars}\n {message}\n{stars}')
+
 def main():
+    banner()
     sentence = input('Enter your sentence:  ')
     output = camelcase(sentence)
     print(output)
